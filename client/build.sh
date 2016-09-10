0a09fd541561ba27951aa1a86da20691dbb28d73
Joshua Lyons <josh@lyonsdensoftware.com>
2016-09-10 15:24:20 -0700

    Deleting client
    Client was not needed for the purposes of the smart mirror.

--- client/build.sh
+++
@@ -1,5 +0,0 @@
-TARGET=/usr/local/i686-netflix-linux-gnu-4.3/bin/i686-netflix-linux-gnu- \
-LDFLAGS="-L/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib \
--Wl,-rpath,/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib" \
-INCLUDES=-I/usr/local/i686-netflix-linux-gnu-4.3/netflix/include \
-make
